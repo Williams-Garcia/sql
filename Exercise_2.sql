9c3335b54ce26764cf3ae58be15bdeefd86b53a9
Williams-Garcia <williams.garcia@mercadolibre.com.mx>
2023-01-24 08:32:48 -0600

    Add concat method for full name actor

--- Exercise_2.sql
+++ Exercise_2.sql
@@ -3,7 +3,7 @@ use movies_db;
 select s.title as titulo, g.name as genero from series s inner join genres g on s.genre_id = g.id;
 
 #	Mostrar el título de los episodios, el nombre y apellido de los actores que trabajan en cada uno de ellos.
-select e.title, a.first_name, a.last_name from episodes e inner join actor_episode ae on e.id = ae.episode_id inner join actors a on ae.actor_id = a.id;
+select e.title, concat(a.first_name, ' ' ,a.last_name) as ActorName from episodes e inner join actor_episode ae on e.id = ae.episode_id inner join actors a on ae.actor_id = a.id;
 
 #Mostrar el título de todas las series y el total de temporadas que tiene cada una de ellas.
 select s.title as titulo, se.serie_id, count(se.number) from series s inner join seasons se on se.serie_id = s.id group by se.serie_id;
@@ -12,4 +12,4 @@ select s.title as titulo, se.serie_id, count(se.number) from series s inner join
 select mo.genre_id, count(*) totalMovies, g.name from movies mo inner join genres g on mo.genre_id = g.id group by mo.genre_id having count(*)>=3;
 
 #Mostrar sólo el nombre y apellido de los actores que trabajan en todas las películas de la guerra de las galaxias y que estos no se repitan.
-select mo.title, ac.first_name, ac.last_name from movies mo inner join actor_movie am on am.movie_id = mo.id inner join actors ac on ac.id = am.actor_id where mo.id = 3 or mo.id = 4 group by mo.title, ac.first_name, ac.last_name;
+select mo.title, concat(ac.first_name, ' ' ,ac.last_name) as ActorName from movies mo inner join actor_movie am on am.movie_id = mo.id inner join actors ac on ac.id = am.actor_id where mo.id = 3 or mo.id = 4 group by mo.title, ac.first_name, ac.last_name;
