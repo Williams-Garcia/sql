583f04a50bdea753d85f6fbd0315fe6a5d15313c
Williams-Garcia <williams.garcia@mercadolibre.com.mx>
2023-01-26 13:33:29 -0600

    Change TRUNCATE for ROUND

--- desafio_bd.sql
+++ desafio_bd.sql
@@ -1,11 +1,11 @@
 use fantasy_products;
 
 -- Montos totales por condition customer
-SELECT TRUNCATE(SUM(s.quantity * p.price), 2) AS total
+SELECT ROUND(SUM(s.quantity * p.price), 2) AS total
 FROM sales s INNER JOIN invoices i ON s.invoice_id = i.id
 INNER JOIN products p ON s.product_id = p.id
 INNER JOIN customers c ON i.customer_id = c.id
-WHERE c.condition = 'True'
+WHERE c.condition = 'False'
 ORDER BY total;
 
 -- Top 5 de los products más vendidos y sus cantidades vendidas
@@ -16,9 +16,9 @@ GROUP BY products.id, products.description
 ORDER BY sales_total DESC LIMIT 5;
 
 -- El Top 5 de los customers activos que gastaron la mayor cantidad de dinero
-SELECT s.product_id, s.invoice_id, s.quantity, i.customer_id, TRUNCATE(SUM(s.quantity * p.price), 2) AS total
+SELECT concat(c.first_name, ' ', c.last_name) as clientName, s.product_id, s.invoice_id, s.quantity, i.customer_id, ROUND(SUM(s.quantity * p.price), 2) AS total
 FROM sales s INNER JOIN invoices i ON s.invoice_id = i.id
 INNER JOIN products p ON s.product_id = p.id
 INNER JOIN customers c ON i.customer_id = c.id
 WHERE c.condition = 'True'
-GROUP BY s.product_id, s.invoice_id, s.quantity, i.customer_id ORDER BY total DESC limit 5;
+GROUP BY s.product_id, s.invoice_id, s.quantity, i.customer_id, clientName ORDER BY total DESC limit 5;
