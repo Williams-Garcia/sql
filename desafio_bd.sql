43939f2e30059695611e2762d0e6c79215886662
Williams-Garcia <williams.garcia@mercadolibre.com.mx>
2023-01-26 13:25:12 -0600

    Querys challenge

---
+++ desafio_bd.sql
@@ -0,0 +1,24 @@
+use fantasy_products;
+
+-- Montos totales por condition customer
+SELECT TRUNCATE(SUM(s.quantity * p.price), 2) AS total
+FROM sales s INNER JOIN invoices i ON s.invoice_id = i.id
+INNER JOIN products p ON s.product_id = p.id
+INNER JOIN customers c ON i.customer_id = c.id
+WHERE c.condition = 'True'
+ORDER BY total;
+
+-- Top 5 de los products más vendidos y sus cantidades vendidas
+SELECT products.id, products.description, SUM(sales.quantity) as sales_total
+FROM products
+INNER JOIN sales ON products.id = sales.product_id
+GROUP BY products.id, products.description
+ORDER BY sales_total DESC LIMIT 5;
+
+-- El Top 5 de los customers activos que gastaron la mayor cantidad de dinero
+SELECT s.product_id, s.invoice_id, s.quantity, i.customer_id, TRUNCATE(SUM(s.quantity * p.price), 2) AS total
+FROM sales s INNER JOIN invoices i ON s.invoice_id = i.id
+INNER JOIN products p ON s.product_id = p.id
+INNER JOIN customers c ON i.customer_id = c.id
+WHERE c.condition = 'True'
+GROUP BY s.product_id, s.invoice_id, s.quantity, i.customer_id ORDER BY total DESC limit 5;
